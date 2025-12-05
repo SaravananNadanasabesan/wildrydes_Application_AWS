c208be15c46198de9b0a4682b45bcc64d46adcf7
8c5BO2VvipedhY <saravanasabesan94@gmail.com>
2025-12-04 20:39:15 -0500

    app updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello from Saravanan Nadanasabesan ECS Fargate!</h1><p>The deployment is successful.</p>"
+    return "<h1>Hello from Saravanan ECS Fargate!</h1><p>The deployment is successful.</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
